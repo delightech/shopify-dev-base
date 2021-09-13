2cd3dc12b96352b208d3c312f049af1498fd2985
delightech <hisafumi.kikkawa@gmail.com>
2021-09-13 12:14:59 +0900

    add polaris

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import enTranslations from '@shopify/polaris/locales/ja.json';
+import {AppProvider, Page, Card, Button} from '@shopify/polaris';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider i18n={enTranslations}>
+    <Page title="Example app">
+      <Card sectioned>
+        <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+      </Card>
+    </Page>
+  </AppProvider>
   );
 }
 
