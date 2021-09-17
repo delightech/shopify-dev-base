56e609847a3ae73674b7b1e6e9eefbcab89276aa
delightech <hisafumi.kikkawa@gmail.com>
2021-09-18 00:32:49 +0900

    一旦warnを消す

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import enTranslations from '@shopify/polaris/locales/ja.json';
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
 
+// eslint-disable-next-line no-alert
+const test = () => alert('Button clicked!');
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const App = () => (
   <AppProvider i18n={enTranslations}>
     <Page title="Example app">
       <Card sectioned>
-        <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+        <Button onClick={test}>Example button</Button>
       </Card>
     </Page>
   </AppProvider>
