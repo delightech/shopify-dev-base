dbc4b456bf0bf3990526f3300e8b033c52ae130d
delightech <hisafumi.kikkawa@gmail.com>
2021-09-17 17:13:54 +0900

    modify lint files

---
+++ .eslintrc.js
@@ -0,0 +1,129 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    // Airbnbが提供する共有設定。広く使われている
+    'airbnb',
+    // 各プラグイン推奨共有設定
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 6,
+    // project: './tsconfig.eslint.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'jest', 'jsx-a11y', 'prefer-arrow', 'react', 'react-hooks'],
+  root: true, // 親ディレクトリの設定ファイルを読み込まないように設定
+  rules: {
+    'sort-imports': 0,
+    'import/order': [2, { alphabetize: { order: 'asc' } }],
+    'newline-before-return': 'warn',
+    'no-console': 'warn',
+    'no-continue': 'off',
+    quotes: [
+      'warn',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    'require-yield': 'error',
+    semi: ['warn', 'always'],
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        markers: ['/'],
+      },
+    ],
+    // @typescript-eslint
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    indent: 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/prefer-interface': 'off',
+    // airbnb
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+    // prefer-arrow
+    'prefer-arrow/prefer-arrow-functions': [
+      'warn',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: true,
+        classPropertiesAllowed: false,
+      },
+    ],
+    // react
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['jsx', 'tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': [
+      'warn',
+      {
+        custom: 'ignore',
+      },
+    ],
+    'react/prop-types': 'off',
+    // react hooks
+    'react-hooks/rules-of-hooks': 'error',
+    // import
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', 'jsx', '.ts', '.tsx'],
+        paths: ['src'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
