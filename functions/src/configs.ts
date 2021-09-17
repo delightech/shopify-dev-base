03a85d9185383b0a98beea4219fa6184160c0fc1
delightech <hisafumi.kikkawa@gmail.com>
2021-09-18 00:48:13 +0900

    modify config path

--- functions/src/configs.ts
+++ functions/src/configs.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 
-export const FIREBASE_FUNCTION_URL = functions.config().delightech.dev.firebase.function.url;
+export const FIREBASE_FUNCTION_URL = functions.config().delightech.dev.firebase.functions.url;
 export const FIREBASE_HOSTING_URL = functions.config().delightech.dev.firebase.hosting.url;
 export const SHOPIFY_API_KEY = functions.config().delightech.dev.shopify.api.key;
 export const SHOPIFY_APP_NAME = functions.config().delightech.dev.shopify.app.name;
