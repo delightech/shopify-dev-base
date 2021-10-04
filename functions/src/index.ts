fee9f076fa47dca76fb57d410cbdb3463b9163f3
delightech <hisafumi.kikkawa@gmail.com>
2021-10-04 16:40:46 +0900

    依存関係とビルド周りの設定を修正& v14系の最新にバージョンアップ

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,28 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import 'firebase-functions';
 import { callbackFromAuth } from './callbackFromAuth';
+import {
+  FIREBASE_PROJECT_ID,
+} from './configs';
+import { verifyHmac } from './verifyHmac';
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-});
+admin.initializeApp();
 
 const testFunc = functions
   .region('asia-northeast1')
   .runWith({ timeoutSeconds: 60, memory: '128MB' })
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   .https.onRequest(async (request, response) => {
     functions.logger.debug('testFunc!!!');
-    console.log('test');
+    functions.logger.debug(request.query);
+    functions.logger.debug(new Date());
+    // 自分のfirebase projectからのアクセスのみ許可する
+    response.set('Access-Control-Allow-Origin', `https://${FIREBASE_PROJECT_ID}.firebaseapp.com`);
     response.send('Hello from Firebase!');
   });
+
 export {
   testFunc,
   callbackFromAuth,
+  verifyHmac,
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import createApp from '@shopify/app-bridge';
 import { Provider } from '@shopify/app-bridge-react';
-import { Redirect } from '@shopify/app-bridge/actions';
 import { AppProvider } from '@shopify/polaris';
 import translations from '@shopify/polaris/locales/ja.json';
+import axios from 'axios';
 import { initializeApp } from 'firebase/app';
-import * as queryString from 'query-string';
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@shopify/polaris/dist/styles.css';
 import { App } from './App';
+import '@shopify/polaris/build/esm/styles.css';
 
 import {
   FIREBASE_API_KEY,
@@ -19,12 +18,10 @@ import {
   SHOPIFY_API_SCOPES,
 } from './configs';
 
-const init = () => {
-  // eslint-disable-next-line no-console
-  console.log(process.env.NODE_ENV);
-  const query = queryString.parse(window.location.search);
-  const shop = query.shop as string;
-  const host = query.host as string;
+const init = async () => {
+  const uri = new URL(window.location.href);
+  const shop = uri.searchParams.get('shop') as string;
+  const host = uri.searchParams.get('host') as string;
   // アプリインストール後、redirect_uriに指定したURLへリダイレクトされる。
   const redirectUri = `${FIREBASE_FUNCTION_URL}/callbackFromAuth`;
   const permissionUrl = `https://${shop}/admin/oauth/authorize?client_id=${SHOPIFY_API_KEY}&scope=${SHOPIFY_API_SCOPES}&redirect_uri=${redirectUri}`;
@@ -34,13 +31,28 @@ const init = () => {
     host: host,
   };
 
+  // アプリインストール時はリダイレクトされてくるので認証へリダイレクトし、
+  // iframeで表示された場合はアプリを表示する。
+  // 認証へリダイレクトする場合はhmac検証を行うのでセキュリティ的には問題ない。
   if (window.top === window.self) {
-    console.log('initialize app!');
-    window.location.assign(permissionUrl);
-  } else {
-    console.log('create app!');
-    const app = createApp(config);
-    Redirect.create(app).dispatch(Redirect.Action.APP, '/');
+    window.location.href = permissionUrl;
+
+    return;
+  }
+  // ここでpermissionUrlへリダイレクトしてしまうとリダイレクトループになるので注意
+
+  console.log('create app!');
+  // propsでコンポーネントにわたしていくのが良さそう。toastなど、app bridgeの機能を使う上で必要
+  const app = createApp(config);
+
+  // functionsでhmac検証をする
+  // hmacの検証はshopify appのsecret keyを利用してサーバーサイドで行う
+  try {
+    await axios.get(`${FIREBASE_FUNCTION_URL}/verifyHmac${window.location.search}`);
+  } catch (e) {
+    console.log(e);
+
+    return;
   }
 
   initializeApp({
@@ -49,13 +61,11 @@ const init = () => {
   });
 
   ReactDOM.render(
-    <React.StrictMode>
-      <AppProvider i18n={translations}>
-        <Provider config={config}>
-          <App />
-        </Provider>
-      </AppProvider>
-    </React.StrictMode>,
+    <AppProvider i18n={translations}>
+      <Provider config={config}>
+        <App />
+      </Provider>
+    </AppProvider>,
     document.getElementById('root'),
   );
 };
